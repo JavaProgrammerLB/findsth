368697a422c3d951acfc69d1d80e54d533baa595
刘备 <admin@yitianyigexiangfa.com>
2017-08-24 17:19:09 +0800

    use multithread

--- findsth.py
+++ findsth.py
@@ -3,6 +3,7 @@ import sys
 import requests
 import logging
 import time
+from multiprocessing.dummy import Pool as ThreadPool
 
 protocol='http'
 topDomainName=['.com','.cn','.net','.de','.fi','.co.jp', '.ru','.me', '.edu', '.af', '.ag', '.ai', '.ar', '.au', '.bd', '.bh', '.bn', '.bo', '.br', '.bz', '.co', '.cu', '.cy', '.do', '.ec', '.eg', '.et', '.fj', '.gh', '.gi', '.gt', '.hk', '.jm', '.kh', '.kw', '.lb', '.ly', '.mm', '.mt', '.mx', '.my', '.na', '.nf', '.ng', '.ni', '.np', '.om', '.pa', '.pe', '.pg', '.ph', '.pk', '.pr', '.py', '.qa', '.sa', '.sb', '.sg', '.sl', '.sv', '.tj', '.tr', '.tw', '.ua', '.uy', '.vc', '.vn']
@@ -11,26 +12,36 @@ print(argv)
 start = time.time()
 
 logging.basicConfig(level=logging.INFO)
+domains = []
 urls=[]
 for i in range(len(topDomainName)):
-	aTop=topDomainName[i]
-	for j in range(len(argv)):
-		domainName=argv[j]
-		url=protocol + '://' + 'www.' + domainName + aTop
-		logging.info('url is: %s',url)
-		try:
-			res=requests.head(url, timeout=10)
-			code=res.status_code
-		except requests.exceptions.ReadTimeout:
-			code=-1
-		except requests.exceptions.ConnectionError:
-			code=-1
-		except requests.exceptions.TooManyRedirects:
-			code=-1
-		logging.info('code is: %d',code) 
-		if code == 200:
-			urls.append(url)
-			logging.info('Available url:%s',url)
+    aTop=topDomainName[i]
+    for j in range(len(argv)):
+        domainName=argv[j]
+        url=protocol + '://' + 'www.' + domainName + aTop
+        logging.info('url is: %s',url)
+        domains.append(url)
+
+print(domains)
+
+def con(url):
+    try:
+        res = requests.get(url, timeout=10)
+        code = res.status_code
+    except requests.exceptions.ReadTimeout:
+        code = -1
+    except requests.exceptions.ConnectionError:
+        code = -1
+    except requests.exceptions.TooManyRedirects:
+        code = -1
+    logging.info('url: %s, code: %d'%(url, code))
+    if code == 200:
+        urls.append(url)
+
+pool = ThreadPool(9)
+pool.map(con, domains)
+pool.close()
+pool.join()
 
 availableCount = len(urls)
 logging.info('availableCount: %d',availableCount)
